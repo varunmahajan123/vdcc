1552eb95d00b4c9c2480267ea8aa1955ff8ebfd4
Varun mahajan <varunmahajan662@gmail.com>
2026-04-24 13:36:22 +0530

    Add sitemap.ts

---
+++ src/app/sitemap.ts
@@ -0,0 +1,29 @@
+
+export default function sitemap() {
+    return [
+        {
+            url: 'https://vdcc-web.vercel.app',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://vdcc-web.vercel.app/about',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://vdcc-web.vercel.app/courses',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://vdcc-web.vercel.app/contact',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://vdcc-web.vercel.app/gallery',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://vdcc-web.vercel.app/results',
+            lastModified: new Date(),
+        }
+    ];
+}
